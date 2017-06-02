3080a6cc3be4fa761644d4dc501a698806727017
cbadal <cbadal@users.noreply.github.com>
2017-06-02 18:54:55 -0400

    k

--- _site/assets/javascripts/randomvideo.js
+++ _site/assets/javascripts/randomvideo.js
@@ -1,4 +1,4 @@
 var videoList = ["assets/video/screenwatch.mp4", "assets/video/fly.mp4", "assets/video/hummingbird.mp4", "assets/video/cherryblossom.mp4", "assets/video/rotation.mp4", "assets/video/grain.mp4", "assets/video/anenome.mp4", "assets/video/eye.mp4"];
 videoList.sort(function(a, b) {return 0.5 - Math.random()});
 
-$("#video").html("<video class='video' autoplay poster='' id='bgvid' loop><source src='" + videoList[0] + "' type='video/mp4'></video>");
+$("#video").html("<video class='video' autoplay playsinline poster='' id='bgvid' loop><source src='" + videoList[0] + "' type='video/mp4'></video>");
