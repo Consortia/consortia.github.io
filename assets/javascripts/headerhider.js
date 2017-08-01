c1e84e4afe21ca0b500aecff8bee3ce3fff54896
cbadal <cbadal@users.noreply.github.com>
2017-08-01 15:50:58 -0400

    k

--- assets/javascripts/headerhider.js
+++ assets/javascripts/headerhider.js
@@ -17,11 +17,11 @@ setInterval(function() {
 
 function hasScrolled() {
     var st = $(this).scrollTop();
-    
+
     // Make sure they scroll more than delta
     if(Math.abs(lastScrollTop - st) <= delta)
         return;
-    
+
     // If they scrolled down and are past the navbar, add class .header-up.
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight){
@@ -33,6 +33,6 @@ function hasScrolled() {
             $('header').removeClass('header-up').addClass('header-down');
         }
     }
-    
+
     lastScrollTop = st;
 }
